bdde21d5428f25582bd7dc235b1876434b61d969
peengtao123 <peengtao123@126.com>
2019-03-29 23:46:43 +0800

    add=init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,49 @@
-function component() {
-    var element = document.createElement('div');
+var d3 = require("d3");
 
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+var width  = 1000;
+var height = 1000;
 
-    return element;
-}
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(0,0)");
 
-document.body.appendChild(component());
+var projection = d3.geo.mercator()
+    .center([107, 31])
+    .scale(850)
+    .translate([width/2, height/2]);
+
+var path = d3.geo.path()
+    .projection(projection);
+
+
+var color = d3.scale.category20();
+
+
+d3.json("china.geojson", function(error, root) {
+
+    if (error)
+        return console.error(error);
+    console.log(root.features);
+
+    svg.selectAll("path")
+        .data( root.features )
+        .enter()
+        .append("path")
+        .attr("stroke","#000")
+        .attr("stroke-width",1)
+        .attr("fill", function(d,i){
+            return color(i);
+        })
+        .attr("d", path )
+        .on("mouseover",function(d,i){
+            d3.select(this)
+                .attr("fill","yellow");
+        })
+        .on("mouseout",function(d,i){
+            d3.select(this)
+                .attr("fill",color(i));
+        });
+
+});
