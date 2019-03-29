955454db1d9b514e1e934416eee135bbe62677f6
peengtao123 <peengtao123@126.com>
2019-03-29 14:44:53 +0800

    add=init

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     mode: "development",
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Custom template',
+            template: 'index.html'
+        })
+    ],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
